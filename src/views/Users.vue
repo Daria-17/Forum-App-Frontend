53f2b08ad1c56c315e490c077439bff2f6006c55
NickMendel <nick.mendel@student.htw-berlin.de>
2022-05-24 10:57:53 +0200

    add userlist to user view

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,10 +1,36 @@
 <template>
   <h1>Members</h1>
+  <div class="member-list" style="position: center">
+    <div class="col" v-for="user in users" :key="user.id">
+      <ul class="list-group list-group-horizontal">
+        <li class="list-group-item">{{ user.id }}</li>
+        <li class="list-group-item">{{ user.nickname }}</li>
+        <li class="list-group-item">{{ user.active ? 'Nutzer: aktiv': 'Nutzer: inaktiv' }}</li>
+      </ul>
+    </div>
+  </div>
+
 </template>
 
 <script>
 export default {
-  name: 'Users'
+  name: 'Users',
+  data () {
+    return {
+      users: [
+        {
+          id: 1,
+          nickname: 'arifWider155',
+          active: 'true'
+        },
+        {
+          id: 2,
+          nickname: 'Goofy1',
+          active: 'false'
+        }
+      ]
+    }
+  }
 }
 </script>
 
