e501a437e0d271c27576bbdab15ede85ae20f5d7
NickMendel <nick.mendel@student.htw-berlin.de>
2022-07-04 23:51:39 +0200

    little fix

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -8,14 +8,10 @@
         <li class="list-group-item">{{ user.id }}</li>
         <li class="list-group-item">{{ user.firstName }}</li>
         <li class="list-group-item">{{ user.lastName }}</li>
-        <li class="list-group-item">{{ user.dob }}</li>
-        <li class="list-group-item">{{ user.username }}</li>
         <li class="list-group-item">{{ user.email }}</li>
         <li class="list-group-item">{{ user.userRole }}</li>
-        <li class="list-group-item">{{ user.creationDate }} </li>
         <li class="list-group-item">{{ user.locked ? 'Nutzer: locked' : 'Nutzer: unlocked' }}</li>
         <li class="list-group-item">{{ user.enabled ? 'Nutzer: aktiv' : 'Nutzer: inaktiv' }}</li>
-
       </ul>
     </div>
   </div>
