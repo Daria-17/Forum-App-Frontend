c4ea30536409a89c460d930b18e48f3525f76efe
NickMendel <nick.mendel@student.htw-berlin.de>
2022-07-04 20:29:43 +0200

    add topics view

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,5 +1,13 @@
 <template>
 <h1>Register</h1>
+  <div class="mb-3" style="position: center">
+      <fieldset name="register">
+        <legend>New Member</legend>
+        <label for="formGroupExampleInput" class="form-label" >Nickname:</label>
+        <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input placeholder" style="position: center;width: 100px" size="">
+      </fieldset>
+    </div>
+
 </template>
 
 <script>
--- src/views/Topics.vue
+++ src/views/Topics.vue
@@ -1,11 +1,40 @@
 <template>
 <h1>Topics</h1>
+  <div class="topic-list">
+    <div class="col" v-for="topic in topics" :key="topic.id">
+      <ul class="list-group list-group-horizontal">
+        <li class="list-group-item">{{ topic.id }}</li>
+        <li class="list-group-item">{{ topic.title }}</li>
+        <li class="list-group-item">{{ topic.active ? 'Topic: aktiv' : 'Topic: inaktiv' }}</li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Topics'
+  name: 'Topics',
+  data () {
+    return {
+      topics: []
+    }
+  },
+  mounted () {
+    const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + '/api/v1/topics'
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    }
+
+    fetch(endpoint, requestOptions)
+      .then(response => response.json())
+      .then(result => result.forEach(topic => {
+        this.topics.push(topic)
+      }))
+      .catch(error => console.log('error', error))
+  }
 }
+
 </script>
 
 <style scoped>
