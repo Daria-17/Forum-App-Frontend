92583ccabb2d6c478fee58290d9bba946574b5a3
Daria-17 <stecenkodasha97@gmail.com>
2022-07-04 20:48:25 +0200

    commit for User and UserCreateForm

---
+++ src/components/UserCreateForm.vue
@@ -0,0 +1,86 @@
+<template>
+  <button class="btn btn-success sticky-button" data-bs-toggle="offcanvas" data-bs-target="#users-create-offcanvas"
+          aria-controls="#users-create-offcanvas">
+    <i class="bi bi-person-plus-fill"></i>
+  </button>
+  <div class="offcanvas offcanvas-end" tabindex="-1" id="users-create-offcanvas" aria-labelledby="offcanvas-label">
+    <div class="offcanvas-header">
+      <h5 id="offcanvas-label">New Members</h5>
+      <button type="button" id="close-offcanvas" class="btn-close text-reset" data-bs-dismiss="offcanvas"
+              aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+      <form class="text-start needs-validation" id="users-create-form" novalidate>
+        <div class="mb-3">
+          <label for="first-name" class="form-label">First name</label>
+          <input type="text" class="form-control" id="first-name" v-model="firstName" required>
+        </div>
+        <div class="mb-3">
+          <label for="last-name" class="form-label">Last name</label>
+          <input type="text" class="form-control" id="last-name" v-model="lastName" required>
+        </div>
+        <div class="mb-3">
+          <label for="last-name" class="form-label">Email</label>
+          <input type="text" class="form-control" id="last-name" v-model="email" required>
+        </div>
+        <div class="mb-3">
+          <label for="last-name" class="form-label">Password</label>
+          <input type="text" class="form-control" id="last-name" v-model="password" required>
+        </div>
+        <div class="mb-3">
+          <label for="gender" class="form-label">User Role</label>
+          <select id="gender" class="form-select" v-model="userRole" required>
+            <option value="" selected disabled>Choose...</option>
+            <option value="MALE">User</option>
+            <option value="FEMALE">Admin</option>
+          </select>
+        </div>
+        <div v-if="this.serverValidationMessages">
+          <ul>
+            <li v-for="(message, index) in serverValidationMessages" :key="index" style="color: red">
+              {{ message }}
+            </li>
+          </ul>
+        </div>
+        <div class="mt-5">
+          <button class="btn btn-primary me-3" type="submit" @click.prevent="createUser">Create</button>
+          <button class="btn btn-danger" type="reset">Reset</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserCreateForm',
+  data () {
+    return {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      userRole: ''
+    }
+  },
+  methods: {
+    createUser () {
+      console.log(this.firstName)
+      console.log(this.lastName)
+      console.log(this.email)
+      console.log(this.password)
+      console.log(this.userRole)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.sticky-button {
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  padding: 10px 15px;
+  border-radius: 30px;
+}
+</style>
--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,11 +1,23 @@
 <template>
   <h1>Members</h1>
   <div class="member-list" >
+    <user-create-form></user-create-form>
     <div class="col" v-for="user in users" :key="user.id">
+
       <ul class="list-group list-group-horizontal">
         <li class="list-group-item">{{ user.id }}</li>
-        <li class="list-group-item">{{ user.nickname }}</li>
+        <li class="list-group-item">{{ user.firstname }}</li>
+        <li class="list-group-item">{{ user.lastname }}</li>
+        <li class="list-group-item">{{ user.dob }}</li>
+        <li class="list-group-item">{{ user.username }}</li>
+        <li class="list-group-item">{{ user.email }}</li>
+        <li class="list-group-item">{{ user.password }}</li>
+        <li class="list-group-item">{{ user.userRole }}</li>
+        <li class="list-group-item">{{ user.creationDate }}</li>
         <li class="list-group-item">{{ user.active ? 'Nutzer: aktiv' : 'Nutzer: inaktiv' }}</li>
+        <li class="list-group-item">{{ user.locked ? 'Nutzer: locked' : 'Nutzer: unlocked' }}</li>
+        <li class="list-group-item">{{ user.enabled ? 'Nutzer: enabled' : 'Nutzer: unenabled' }}</li>
+
       </ul>
     </div>
   </div>
@@ -13,11 +25,45 @@
 </template>
 
 <script>
+import UserCreateForm from '@/components/UserCreateForm'
 export default {
   name: 'Users',
+  components: {
+    UserCreateForm
+  },
   data () {
     return {
-      users: []
+      users: [
+        {
+          id: 1,
+          firstname: 'Maya',
+          lastname: 'Meier',
+          dob: 2001 - 12 - 6,
+          username: 'ingo',
+          email: 'stnh2@gmail.com',
+          password: '1256495we',
+          userRole: 'user',
+          creationDate: 2022 - 15 - 9,
+          active: true,
+          locked: true,
+          enabled: true
+        },
+        {
+          id: 2,
+          firstname: 'Lina',
+          lastname: 'Miren',
+          dob: 1986 - 10 - 25,
+          username: 'lina287',
+          email: 'miren25@gmail.com',
+          password: '1248rt156.',
+          userRole: 'user',
+          creationDate: 2022 - 7 - 7,
+          active: true,
+          locked: true,
+          enabled: true
+
+        }
+      ]
     }
   },
   mounted () {
